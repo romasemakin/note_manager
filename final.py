47760e573afdcebb7a914a152c11e538975eb49d
romasemakin <65649055+romasemakin@users.noreply.github.com>
2024-12-13 17:24:17 +0400

    1

--- final.py
+++ final.py
@@ -6,13 +6,18 @@ status = input("Введите статус заметки: ")
 created_date = input("Введите Дату создания в формате день, месяц ,год (10-10-2000): ")
 issue_date = input("Введите Дату истечения заметки в формате день, месяц ,год (10-10-2000): ")
 
+titles = [
+fitst_title, 
+second_title
+]
+
 note_ = [
 username, # indx = 0
 content, # indx = 1
 status, # indx = 2
 created_date, # indx = 3
 issue_date, # indx = 4
-[fitst_title, second_title] # вложенный список для заголовков indx = 5
+titles # вложенный список для заголовков indx = 5
 ]
 
 print("Здрастуйте,", note_[0])
