4e266a2e97fe14341980052454c5c89b2a69c92e
romasemakin <semakin199@gmail.com>
2024-12-25 11:10:17 +0400

    1

--- grade 1 этап 2/add_titles_loop.py
+++ grade 1 этап 2/add_titles_loop.py
@@ -4,8 +4,18 @@ runorno = "Да"
 while True:
     titles.append(input("Введите заголовок: "))
     print(titles[-1])
+
     runorno = input("желаете добавить ещё заголовок? Да/Нет: ")
-    if runorno == "Нет":
+
+    if runorno == "Да":
+        continue
+    else:   
+        if runorno == "Нет":
+            break
+        
+    if runorno != "Да" or "Нет":
+        runorno = input("Введите Да/Нет: ")
+    else:
         break
 
 print("Все заголовки: ", titles)
