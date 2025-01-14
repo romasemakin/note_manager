935ed56a4537a4869fef6a2fa8d880cfe6e8c54d
romasemakin <65649055+romasemakin@users.noreply.github.com>
2025-01-14 13:30:55 +0400

    Delete grade 1 этап 1 directory

--- grade 1 этап 1/date_changer.py
+++
@@ -1,7 +0,0 @@
-created_date = "10-11-2024" 
-issue_date = "10-12-2024"
-temp_created_date = created_date[:5]
-temp_issue_date = issue_date[:5]
-
-print("Дата создания: ", temp_created_date)
-print("Дата истечения заметки: ", temp_issue_date)
