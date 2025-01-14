02c26fb9fc1d46f62ddb1b686be2117d5b0b90a1
romasemakin <65649055+romasemakin@users.noreply.github.com>
2025-01-14 13:32:05 +0400

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,13 @@
+username = input("Введите имя: ")
+title = input("Введите заголовок: ")
+content = input("Введите описание: ")
+status = input("Введите статус заметки: ")
+created_date = input("Введите Дату создания в формате день, месяц ,год (10-10-2000): ")
+issue_date = input("Введите Дату истечения заметки в формате день, месяц ,год (10-10-2000): ")
+
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Описание:", content)
+print("Статус:", status)
+print("Дата создания:", created_date)
+print("Дата истечения заметки:", issue_date)
---
+++ add_list.py
@@ -0,0 +1,9 @@
+fitst_title =  input("Введите первый заголовок: ")
+second_title = input("Введите второй заголовок: ")
+third_title = input("Введите третий заголовок: ")
+title_ = [fitst_title, second_title, third_title]
+
+print("Первый заголовок: ", title_[0])
+print("Второй заголовок: ", title_[1])
+print("Третий заголовок: ", title_[2])
+print("Все заголовоки: ", title_)
